fb2460ed9d8a509c701c62bb668d8885c2059782
RushabhaJain <rushabhajain44@gmail.com>
2022-10-21 18:41:26 +0530

    Added code for extracting audio from video and merging video with subtitle

---
+++ ffmpeg/ffmpeg.go
@@ -0,0 +1,67 @@
+package ffmpeg
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func ExtractAudio(videoFilePath, audioFilePath string) error {
+
+	fmt.Printf("Extracting audio out of %v...\n", videoFilePath)
+
+	cmd := exec.Command(
+		"ffmpeg",
+		"-i",
+		videoFilePath,
+		"-f",
+		"mp3",
+		"-ab",
+		"19200",
+		"-y",
+		"-vn",
+		audioFilePath,
+	)
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error while extracting audio from video")
+		fmt.Println(stderr.String())
+		return err
+	}
+
+	fmt.Printf("Successfully extraced audio out of %v!\n", videoFilePath)
+	return nil
+}
+
+func MergeSubtitle(subTitleFilePath, videoFilePath string) (string, error) {
+	// outputFilePath := filepath.Join(filepath.Dir(videoFilePath), time.Now().String()+".mp4")
+	outputFilePath := filepath.Join(filepath.Dir(videoFilePath), strings.TrimSuffix(filepath.Base(videoFilePath), filepath.Ext(videoFilePath))+"_output"+filepath.Ext(videoFilePath))
+	cmd := exec.Command(
+		"ffmpeg",
+		"-i",
+		videoFilePath,
+		"-filter_complex",
+		fmt.Sprintf("subtitles=%v", filepath.Base(subTitleFilePath)),
+		outputFilePath,
+	)
+
+	cmd.Dir = filepath.Dir(subTitleFilePath)
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error while extracting audio from video")
+		fmt.Println(stderr.String())
+		return "", err
+	}
+
+	return outputFilePath, nil
+}
